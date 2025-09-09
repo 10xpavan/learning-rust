cab802d7ae491a60d35551554c387ea20ba60412
10xpavan <pavan.nulenur@gmail.com>
2025-09-09 22:01:06 +0530

    command line 0.2

--- ripgerp/src/main.rs
+++ ripgerp/src/main.rs
@@ -1,6 +1,18 @@
-use std::env;
-
+use std::env;         //this is used to interact with the engivornment
+use std::fs;
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let args: Vec<String> = env::args().collect();         //this thing collects the arguments over the command line and sends it to store in the verctorized string format
+    
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In the file {file_path}");
+    
+    
+    println!("in file heheboi");
+    let contents = fs::read_to_string("B:\\notes\\rust-world\\ripgerp\\heheboi.txt")
+         .expect("should have been able to read the file");
+
+    println!("with text:\n{contents}");
 }
