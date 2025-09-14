d8383879119b94477fa7eb907cd42d499703934d
10xpavan <pavan.nulenur@gmail.com>
2025-09-14 16:45:01 +0530

    progress maxxing on CLI

--- ripgerp/src/main.rs
+++ ripgerp/src/main.rs
@@ -3,16 +3,23 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();         //this thing collects the arguments over the command line and sends it to store in the verctorized string format
     
+    //let query = &args[1];
+    //let file_path = &args[2];
+
+    //println!("Searching for {query}");
+    //println!("In the file {file_path}");
+    
+    
+    //println!("in file heheboi");
+    //let contents = fs::read_to_string("B:\\notes\\rust-world\\ripgerp\\heheboi.txt")
+         //.expect("should have been able to read the file");
+
+    //println!("with text:\n{contents}");
+    let (query, file_path) = prase_config(&args);
+}
+
+fn prase_config(args: &[String]) -> (&str, &str) {
     let query = &args[1];
     let file_path = &args[2];
-
-    println!("Searching for {query}");
-    println!("In the file {file_path}");
-    
-    
-    println!("in file heheboi");
-    let contents = fs::read_to_string("B:\\notes\\rust-world\\ripgerp\\heheboi.txt")
-         .expect("should have been able to read the file");
-
-    println!("with text:\n{contents}");
+    (query, file_path)
 }
